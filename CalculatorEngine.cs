bd81c087e15471e37a467e97f56770fa7bf52c14
N1njaTortoise <52889916+N1njaTortoise@users.noreply.github.com>
2020-06-15 19:36:15 +0200

    Add project files.

---
+++ CalculatorEngine.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    class CalculatorEngine
+    {
+        private double input1;
+        private double input2;
+        private string operation;
+        private double sum;
+
+        public void CalculatorStart()
+        {
+            while (true)
+            {
+                UserInput();
+
+                sum = Calculate();
+
+                Console.WriteLine($"The sum of {input1}{operation}{input2}={sum}");
+            }
+        }
+
+        private void UserInput()
+        {
+            Console.Write("Enter a number: ");
+            input1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter another number: ");
+            input2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the type of operation to do (+, -, *, /): ");
+            operation = Console.ReadLine();
+
+
+        }
+
+        private double Calculate()
+        {
+            switch (operation)
+            {
+                case "+":
+                    return input1 + input2;
+                case "-":
+                    return input1 - input2;
+                case "*":
+                    return input1 * input2;
+                case "/":
+                    return input1 / input2;
+                default:
+                    Console.WriteLine("The incorrect operation type was used.");
+                    return 0;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculatorEngine start = new CalculatorEngine();
+
+            start.CalculatorStart();
+        }
+    }
+}
